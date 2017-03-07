39fcdeb692decc8c7b1422f6c0a4678b58bc04d0
jGoldenko <jgold6001@gmail.com>
2017-03-07 13:15:17 +0200

    AdminWindow and PersonalWindow Made

---
+++ 01 - WpfWindows/PersonalWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _01___WpfWindows
+{
+    /// <summary>
+    /// Interaction logic for PersonalWindow.xaml
+    /// </summary>
+    public partial class PersonalWindow : Window
+    {
+        public PersonalWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
