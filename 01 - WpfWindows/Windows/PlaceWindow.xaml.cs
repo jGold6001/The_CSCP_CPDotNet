f72b529d6f32db446d8f4ffb0e513e60a7b90b90
jGoldenko <jgold6001@gmail.com>
2017-03-10 13:01:39 +0200

    created IndividualStyles

--- 01 - WpfWindows/Windows/PlaceWindow.xaml.cs
+++ 01 - WpfWindows/Windows/PlaceWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace _01___WpfWindows
         {
             InitializeComponent();
             listBtns = new List<Button>();
-            stylePlace = this.FindResource("btnPayDeposit") as Style;
+            stylePlace = this.FindResource("btnOrange") as Style;
             UploadGgidVertical(gridSection1, 14, 1);
             UploadGgidVertical(gridSection2, 10, 16);
             UploadGgidVertical(gridSection3, 10, 26);
